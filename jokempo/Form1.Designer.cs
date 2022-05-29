7eae683649468e918733afc23cb03d1657d3d493
michael <michaelneves800@gmail.com>
2022-05-29 13:48:00 -0300

    Add project files.

---
+++ jokempo/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace jokempo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labeltitulo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureresultado = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BTNPEDRA = new System.Windows.Forms.Button();
+            this.btnpapel = new System.Windows.Forms.Button();
+            this.btntesoura = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureresultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labeltitulo
+            // 
+            this.labeltitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labeltitulo.Location = new System.Drawing.Point(12, 9);
+            this.labeltitulo.Name = "labeltitulo";
+            this.labeltitulo.Size = new System.Drawing.Size(776, 104);
+            this.labeltitulo.TabIndex = 0;
+            this.labeltitulo.Text = "Escolha: Pedra, Papel ou Tesoura";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(40, 117);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(306, 117);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureresultado
+            // 
+            this.pictureresultado.Location = new System.Drawing.Point(572, 117);
+            this.pictureresultado.Name = "pictureresultado";
+            this.pictureresultado.Size = new System.Drawing.Size(200, 200);
+            this.pictureresultado.TabIndex = 3;
+            this.pictureresultado.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(110, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "você";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(388, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PC";
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.AutoSize = true;
+            this.labelResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResultado.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.labelResultado.Location = new System.Drawing.Point(662, 198);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(25, 25);
+            this.labelResultado.TabIndex = 6;
+            this.labelResultado.Text = "?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(255, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "vs";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label5.Location = new System.Drawing.Point(527, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "=";
+            // 
+            // BTNPEDRA
+            // 
+            this.BTNPEDRA.BackgroundImage = global::jokempo.Properties.Resources.Pedra;
+            this.BTNPEDRA.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BTNPEDRA.Location = new System.Drawing.Point(40, 340);
+            this.BTNPEDRA.Name = "BTNPEDRA";
+            this.BTNPEDRA.Size = new System.Drawing.Size(200, 200);
+            this.BTNPEDRA.TabIndex = 9;
+            this.BTNPEDRA.UseVisualStyleBackColor = true;
+            this.BTNPEDRA.Click += new System.EventHandler(this.BTNPEDRA_Click);
+            // 
+            // btnpapel
+            // 
+            this.btnpapel.BackgroundImage = global::jokempo.Properties.Resources.Papel;
+            this.btnpapel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnpapel.Location = new System.Drawing.Point(306, 340);
+            this.btnpapel.Name = "btnpapel";
+            this.btnpapel.Size = new System.Drawing.Size(200, 200);
+            this.btnpapel.TabIndex = 10;
+            this.btnpapel.UseVisualStyleBackColor = true;
+            this.btnpapel.Click += new System.EventHandler(this.btnpapel_Click);
+            // 
+            // btntesoura
+            // 
+            this.btntesoura.BackgroundImage = global::jokempo.Properties.Resources.Tesoura;
+            this.btntesoura.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btntesoura.Location = new System.Drawing.Point(572, 340);
+            this.btntesoura.Name = "btntesoura";
+            this.btntesoura.Size = new System.Drawing.Size(200, 200);
+            this.btntesoura.TabIndex = 11;
+            this.btntesoura.UseVisualStyleBackColor = true;
+            this.btntesoura.Click += new System.EventHandler(this.btntesoura_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 552);
+            this.Controls.Add(this.btntesoura);
+            this.Controls.Add(this.btnpapel);
+            this.Controls.Add(this.BTNPEDRA);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureresultado);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labeltitulo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureresultado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labeltitulo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureresultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BTNPEDRA;
+        private System.Windows.Forms.Button btnpapel;
+        private System.Windows.Forms.Button btntesoura;
+    }
+}
+
---
+++ jokempo/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jokempo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTNPEDRA_Click(object sender, EventArgs e)
+        {
+            StartGame(0);
+        }
+
+        private void btnpapel_Click(object sender, EventArgs e)
+        {
+            StartGame(2);
+        }
+
+        private void btntesoura_Click(object sender, EventArgs e)
+        {
+            StartGame(1);
+        }
+
+        private void StartGame(int opcao)
+        {
+            labelResultado.Visible = false;
+            Game jogo = new Game();
+
+            switch (jogo.Jogar(opcao))
+            {
+                case Game.Resultado.Ganhar:
+                    pictureresultado.BackgroundImage = Image.FromFile("imagens/Ganhar.png");
+                    goto default;
+                case Game.Resultado.Perder:
+                    pictureresultado.BackgroundImage = Image.FromFile("imagens/Perder.png");
+                    goto default;
+                case Game.Resultado.Empatar:
+                    pictureresultado.BackgroundImage = Image.FromFile("imagens/Empatar.png");
+                    goto default;
+                default:                    
+
+                    pictureBox1.Image = jogo.ImgJogador;
+                    pictureBox2.Image = jogo.ImgPc;
+                    break;
+
+            }
+        }
+    }
+}
